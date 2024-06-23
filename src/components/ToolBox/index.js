b9dbdec6b4b0a762a11955fd4a045b6762a0b95a
shiv <codewithshiv194@gmail.com>
2024-06-23 15:25:07 +0530

    fillcolor bug fixed

--- src/components/ToolBox/index.js
+++ src/components/ToolBox/index.js
@@ -23,106 +23,114 @@ const ToolBox = () => {
   const strokeColor = toolboxState[activeToolItem]?.stroke;
   const fillColor = toolboxState[activeToolItem]?.fill;
   const size = toolboxState[activeToolItem].size;
-  return (activeToolItem!=TOOL_ITEMS.ERASER &&
-    <div className={classes.container}>
-      <div className={classes.selectOptionContainer}>
-        {STROKE_TOOL_TYPES.includes(activeToolItem) && (
-          <>
-            <div className={classes.toolBoxLabel}>Stroke Color</div>
-            <div className={classes.colorsContainer}>
-            <div>
-              <input
-                className={classes.colorPicker}
-                type="color"
-                value={strokeColor}
-                onChange={(e) => handleStrokeChange(activeToolItem, e.target.value)}
-              ></input>
-            </div>
-              {Object.keys(COLORS).map((key) => {
-                return (
+  return (
+    activeToolItem != TOOL_ITEMS.ERASER && (
+      <div className={classes.container}>
+        <div className={classes.selectOptionContainer}>
+          {STROKE_TOOL_TYPES.includes(activeToolItem) && (
+            <>
+              <div className={classes.toolBoxLabel}>Stroke Color</div>
+              <div className={classes.colorsContainer}>
+                <div>
+                  <input
+                    className={classes.colorPicker}
+                    type="color"
+                    value={strokeColor}
+                    onChange={(e) =>
+                      handleStrokeChange(activeToolItem, e.target.value)
+                    }
+                  ></input>
+                </div>
+                {Object.keys(COLORS).map((key) => {
+                  return (
+                    <div
+                      key={key}
+                      className={cx(classes.colorBox, {
+                        [classes.activeColorBox]:
+                          COLORS[key] === toolboxState[activeToolItem].stroke,
+                      })}
+                      style={{ backgroundColor: COLORS[key] }}
+                      onClick={() =>
+                        handleStrokeChange(activeToolItem, COLORS[key])
+                      }
+                    ></div>
+                  );
+                })}
+              </div>
+            </>
+          )}
+
+          {FILL_TOOL_TYPES.includes(activeToolItem) && (
+            <>
+              <br />
+              <div className={classes.toolBoxLabel}>Fill Color</div>
+              <div className={classes.colorsContainer}>
+                {fillColor === null ? (
                   <div
-                    key={key}
-                    className={cx(classes.colorBox, {
-                      [classes.activeColorBox]:
-                        COLORS[key] === toolboxState[activeToolItem].stroke,
-                    })}
-                    style={{ backgroundColor: COLORS[key] }}
+                    className={cx(classes.colorPicker, classes.noFillColorBox)}
                     onClick={() =>
-                      handleStrokeChange(activeToolItem, COLORS[key])
+                      handleFillChange(activeToolItem, COLORS.BLACK)
                     }
                   ></div>
-                );
-              })}
-            </div>
-          </>
-        )}
-        
-        {FILL_TOOL_TYPES.includes(activeToolItem) && (
-          <>
-            <br />
-            <div className={classes.toolBoxLabel}>Fill Color</div>
-            <div className={classes.colorsContainer}>
-            {fillColor === null ? (
-              <div
-                className={cx(classes.colorPicker, classes.noFillColorBox)}
-                onClick={() => handleFillChange(activeToolItem, COLORS.BLACK)}
-              ></div>
-            ) : (
-              <div>
+                ) : (
+                  <div>
+                    <input
+                      className={classes.colorPicker}
+                      type="color"
+                      value={fillColor}
+                      onChange={(e) =>
+                        handleFillChange(activeToolItem, e.target.value)
+                      }
+                    ></input>
+                  </div>
+                )}
+                <div
+                  className={cx(classes.colorBox, classes.noFillColorBox, {
+                    [classes.activeColorBox]: fillColor === null,
+                  })}
+                  onClick={() => handleFillChange(activeToolItem, null)}
+                ></div>
+                {Object.keys(COLORS).map((key) => {
+                  return (
+                    <div
+                      key={key}
+                      className={cx(classes.colorBox, {
+                        [classes.activeColorBox]:
+                          COLORS[key] === toolboxState[activeToolItem].fill,
+                      })}
+                      style={{ backgroundColor: COLORS[key] }}
+                      onClick={() =>
+                        handleFillChange(activeToolItem, COLORS[key])
+                      }
+                    ></div>
+                  );
+                })}
+              </div>
+            </>
+          )}
+          <br />
+          {SIZE_TOOL_TYPES.includes(activeToolItem) && (
+            <>
+              <div className={classes.toolBoxLabel}>Size </div>
+              <div className={classes.colorsContainer}>
                 <input
-                  className={classes.colorPicker}
-                  type="color"
-                  value={strokeColor}
-                  onChange={(e) => handleFillChange(activeToolItem, e.target.value)}
+                  type="range"
+                  // min={activeToolItem === TOOL_ITEMS.TEXT ? 12 : 1}
+                  // max={activeToolItem === TOOL_ITEMS.TEXT ? 64 : 10}
+                  min={activeToolItem === TOOL_ITEMS.TEXT ? 12 : 1}
+                  max={activeToolItem === TOOL_ITEMS.TEXT ? 64 : 10}
+                  step={1}
+                  value={size}
+                  onChange={(event) =>
+                    handleChangeSize(activeToolItem, event.target.value)
+                  }
                 ></input>
               </div>
-            )}
-            <div
-              className={cx(classes.colorBox, classes.noFillColorBox, {
-                [classes.activeColorBox]: fillColor === null,
-              })}
-              onClick={() => handleFillChange(activeToolItem, null)}
-            ></div>
-              {Object.keys(COLORS).map((key) => {
-                return (
-                  <div
-                    key={key}
-                    className={cx(classes.colorBox, {
-                      [classes.activeColorBox]:
-                        COLORS[key] === toolboxState[activeToolItem].fill,
-                    })}
-                    style={{ backgroundColor: COLORS[key] }}
-                    onClick={() =>
-                      handleFillChange(activeToolItem, COLORS[key])
-                    }
-                  ></div>
-                );
-              })}
-            </div>
-          </>
-        )}
-        <br />
-        {SIZE_TOOL_TYPES.includes(activeToolItem) && (
-          <>
-            <div className={classes.toolBoxLabel}>Size </div>
-            <div className={classes.colorsContainer}>
-              <input
-                type="range"
-                // min={activeToolItem === TOOL_ITEMS.TEXT ? 12 : 1}
-                // max={activeToolItem === TOOL_ITEMS.TEXT ? 64 : 10}
-                min={activeToolItem === TOOL_ITEMS.TEXT ? 12 : 1}
-                max={activeToolItem === TOOL_ITEMS.TEXT ? 64 : 10}
-                step={1}
-                value={size}
-                onChange={(event) =>
-                  handleChangeSize(activeToolItem, event.target.value)
-                }
-              ></input>
-            </div>
-          </>
-        )}
+            </>
+          )}
+        </div>
       </div>
-    </div>
+    )
   );
 };
 export default ToolBox;
